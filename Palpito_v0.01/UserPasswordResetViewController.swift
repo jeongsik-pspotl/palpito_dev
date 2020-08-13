224ddf62a7d2c1ee649d8b340e67c83df52ec69d
김정식 <kimjs10122@naver.com>
2020-08-13 22:20:35 +0900

    비밀번호 재설정 Controller 기능 및 구성 추가

--- Palpito_v0.01/UserPasswordResetViewController.swift
+++ Palpito_v0.01/UserPasswordResetViewController.swift
@@ -7,15 +7,56 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class UserPasswordResetViewController: UIViewController {
+class UserPasswordResetViewController: ExtensionVC {
 
+    @IBOutlet weak var emailCheckTextFeled:UITextField?
+    
+    @IBOutlet weak var passwordResetView: UIView!
+    
+    var sv:UIView = UIView.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scale = view.bounds.width / passwordResetView.bounds.width
+        
+        passwordResetView.transform = CGAffineTransform(scaleX: scale, y: scale)
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func passwordResetfromEmailSendAction(){
+        
+        //self.sv = UIViewController.displaySpinner(onView: self.view)
+        
+        guard let emailcheck = self.emailCheckTextFeled?.text else {
+            return
+        }
+        
+        
+        Auth.auth().sendPasswordReset(withEmail: emailcheck) { (err) in
+            if err == nil {
+                
+                let storyboard = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StartViewSb") as! ViewController
+                storyboard.modalPresentationStyle = .fullScreen
+                
+                self.present(storyboard, animated: false, completion: {
+                    // self.sv.removeFromSuperview()
+                })
+                
+                
+            }else {
+                
+            }
+        }
+        
+    }
+    
+    //  확인 버튼
+    // email input
+    // email check 정규식 추가
 
     /*
     // MARK: - Navigation
